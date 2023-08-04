3168b3ae59f72a374cb3cd5bf3d0eaf200d76748
Андрей Мартынов <real.cloudhunter@gmail.com>
2023-08-04 17:21:39 +0300

    TURN OFF .env USING. TRY TU USE GITHUB ENV.

--- poetry_study_app/settings.py
+++ poetry_study_app/settings.py
@@ -9,15 +9,17 @@ class Settings(BaseSettings):
     status_url: str
 
 
-@lru_cache
-def get_settings():
-    """ Add .env as environment source file"""
-    appsettings = Settings(
-        _env_file="poetry_study_app/.env",
-        _env_file_encoding="utf-8"
-    )
-    return appsettings
+# @lru_cache
+# def get_settings():
+#     """ Add .env as environment source file"""
+#     appsettings = Settings(
+#         _env_file="poetry_study_app/.env",
+#         _env_file_encoding="utf-8"
+#     )
+#     return appsettings
+#
+#
+# app_settings = get_settings()
 
-
-app_settings = get_settings()
+app_settings = Settings()
 logger.debug(f"{app_settings = }")
