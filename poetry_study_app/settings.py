44f3b6e60d70ef8b69d8ceadc58b51df455011df
Андрей Мартынов <real.cloudhunter@gmail.com>
2023-08-07 00:41:15 +0300

    Solve some PyLint warnings in settings.py

--- poetry_study_app/settings.py
+++ poetry_study_app/settings.py
@@ -1,38 +1,17 @@
 """ App settings file """
-from functools import lru_cache
-
-from pydantic import ValidationError, Field
-from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic import ValidationError
+from pydantic_settings import BaseSettings
 from loguru import logger
 
 
 class Settings(BaseSettings):
     """ App settings storage class """
-    # model_config = SettingsConfigDict(
-    #     env_file='.env',
-    #     env_file_encoding='utf-8'
-    # )
     status_url: str
 
 
-# @lru_cache
-# def get_settings():
-#     """ Add .env as environment source file"""
-#     appsettings = Settings(
-#         _env_file=".env",
-#         _env_file_encoding="utf-8"
-#     )
-#     logger.debug(f"{appsettings = }")
-#     return appsettings
-
-
-logger.debug(f"Try to get settings")
-# app_settings = get_settings()
+logger.debug("Try to get settings")
 try:
     app_settings = Settings()
 except ValidationError as v_ex:
     logger.error(f"{v_ex.errors()}")
     raise v_ex
-
-# app_settings = Settings(_env_file='.env', _env_file_encoding='utf-8')
-logger.debug(f"{app_settings = }")
