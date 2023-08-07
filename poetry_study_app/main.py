0a3771b8fd572cbe4df5e64f2730bf5b968a870e
Андрей Мартынов <real.cloudhunter@gmail.com>
2023-08-07 23:26:57 +0300

    Drop tmp logging, add startup tips

--- poetry_study_app/main.py
+++ poetry_study_app/main.py
@@ -1,7 +1,6 @@
 """ Main module """
 from fastapi import FastAPI
 from pydantic import BaseModel
-from loguru import logger
 from poetry_study_app.settings import app_settings
 
 
@@ -16,5 +15,4 @@ class Status(BaseModel):
 @app.get(app_settings.status_url, tags=['Example routers'])
 async def status() -> Status:
     """ Status request func """
-    logger.debug(f"{app_settings.status_url = }")
     return Status()
--- poetry_study_app/settings.py
+++ poetry_study_app/settings.py
@@ -9,9 +9,11 @@ class Settings(BaseSettings):
     status_url: str
 
 
-logger.debug("Try to get settings")
+logger.info("Example container run command: docker run --name Poetry_Study "
+            "--rm -e status_url='/status' -p 8888:8888 <image_ID>")
 try:
     app_settings = Settings()
 except ValidationError as v_ex:
-    logger.error(f"{v_ex.errors()}")
+    logger.error("[!] Project environment variable for '/status' endpoint not found\n"
+                 "[!] add -e status_url='/status' into docker run string/")
     raise v_ex
